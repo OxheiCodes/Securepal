198f5c64edb5ab9f288c3084d80ab2b9fc6af1a0
OxheiCodes <oxhei212@gmail.com>
2025-03-11 22:36:22 +0000

    Backend Setup 9:56

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router(); // this is the router object that will be used to define the routes
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+
+export default router; // this exports the router object to be used in the server.js file
---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import authRoutes from './routes/auth.route.js';
+
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/v1/auth", authRoutes) // this is the route for the auth routes including versioning of the api "/api/v1/auth"
+
+app.listen(PORT, () => {
+
+    console.log(`Server is running on port ${PORT}`);
+})
